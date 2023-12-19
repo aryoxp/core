736bec791547597be12095ec1d68994c631afbbf
Aryo Pinandito <aryoxp@gmail.com>
2023-12-19 11:17:56 +0700

    Bug fix unmark from stop (also interchange)

--- admin/module/mta/service/StopService.php
+++ admin/module/mta/service/StopService.php
@@ -3,11 +3,13 @@
 class StopService extends CoreService {
 
   public function setStop($id, $isStop = true) {
+    $update['stop'] = $isStop ? '1' : '0';
+    if (!$isStop) $update['idinterchange'] = null;
+
     $db = self::instance('mta');
     $qb = QB::instance('linepoint l')
-      ->update('stop', $isStop ? '1' : '0');
-    if (!$isStop) $qb->update('idinterchange', null);
-    $qb->where('idpoint', QB::esc($id));
+      ->update($update)
+      ->where('idpoint', QB::esc($id));
     return $db->query($qb->get());
   }
 
