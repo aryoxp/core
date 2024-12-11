ed2a09577146d9b93689cbb41dd2609016719608
Aryo Pinandito <aryoxp@gmail.com>
2024-12-12 05:10:38 +0700

    Removing folder wmon

--- admin/module/wmon/view/test.php
+++
@@ -1,2 +0,0 @@
-<img src="<?php echo $this->asset('logo-whn.png', 'admin/module/wmon/asset/image/'); ?>" style="width:5cm">
-<p style="font-family: 'Fira Sans Condensed'; font-size: 10pt;"> This is a hello world</p>
